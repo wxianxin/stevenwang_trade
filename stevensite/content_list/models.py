fe7656394f9f36373315a8a0e67dd4a4e9fef1e2
wxianxin <wxianxinreg@gmail.com>
2017-11-02 10:35:44 -0400

    add features to content_list; ready to move

--- stevensite/content_list/models.py
+++ stevensite/content_list/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Article(models.Model):
-    title_text = models.CharField(max_length=200)
+    category_text = models.CharField(max_length=50, default='category')
+    name_text = models.CharField(max_length=20, default='code_name')
+    title_text = models.CharField(max_length=200, default='title')
+    subtitle_text = models.CharField(max_length=200, default='subtitle')
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
--- stevensite/content_list/views.py
+++ stevensite/content_list/views.py
@@ -6,10 +6,16 @@ from .models import Article
 
 class IndexView(generic.ListView):
     template_name = 'content_list/index.html'
+    category = Article
     # context_object_name = 'article_list'
 
     def get_queryset(self):
         """get titles of every article"""
         return Article.objects.all()
+    
 
+    #def get_category(self):
+    #    first_article = Article.objects.get(pk=1)
+    #    category = first_article.category_text
+    #    return category
 
